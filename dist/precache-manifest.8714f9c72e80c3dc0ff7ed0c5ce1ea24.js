c4d5be8a4e20bb70f2e15f4d50535bb9b10753bd
Mem U Sins <83538724+memUsins@users.noreply.github.com>
2022-06-29 14:56:16 +0300

    Heroku

---
+++ dist/precache-manifest.8714f9c72e80c3dc0ff7ed0c5ce1ea24.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b6972f4b44c830c9ce4",
+    "url": "/css/app.ce5e6aec.css"
+  },
+  {
+    "revision": "605e5bb895a59080f141d4c2e6447407",
+    "url": "/fonts/PT-Mono_Bold.woff"
+  },
+  {
+    "revision": "844e8fa6bb3441effec73e976764c535",
+    "url": "/fonts/PTMono-Regular.ttf"
+  },
+  {
+    "revision": "5a0b620543e9cc4b1232b1a616c6c2b5",
+    "url": "/fonts/TTOctosquares-Black.ttf"
+  },
+  {
+    "revision": "4134316262d373f278cfd2d9f431bdb0",
+    "url": "/fonts/TTOctosquares-Bold.ttf"
+  },
+  {
+    "revision": "fbdd0e87900ffb49993e853cb25f603c",
+    "url": "/fonts/TTOctosquares-Light.ttf"
+  },
+  {
+    "revision": "9ebb13e20971663062cfda654709404e",
+    "url": "/fonts/TTOctosquares-Medium.ttf"
+  },
+  {
+    "revision": "d9fed18c694e4dcd885ffef2e0642a91",
+    "url": "/fonts/TTOctosquares-Regular.ttf"
+  },
+  {
+    "revision": "2353de02ed953ee562e086d5926b0ce1",
+    "url": "/fonts/TTOctosquares-Thin.ttf"
+  },
+  {
+    "revision": "fd485a153be673a2c7a322c12e41af9c",
+    "url": "/fonts/fonts.css"
+  },
+  {
+    "revision": "aedde50d986d262a014a5eb6f4c3f75b",
+    "url": "/img/planets/1.png"
+  },
+  {
+    "revision": "0a487381b768c8c7894a3fc13fbc02cd",
+    "url": "/img/planets/10.png"
+  },
+  {
+    "revision": "c2e6293d280a6b5229df5acbc357033c",
+    "url": "/img/planets/11.png"
+  },
+  {
+    "revision": "a5a631e889df8252dc4be8ae05127860",
+    "url": "/img/planets/12.png"
+  },
+  {
+    "revision": "cd54fb81852a6c2c990c05eb9e2e2756",
+    "url": "/img/planets/13.png"
+  },
+  {
+    "revision": "80ed2528dd34a37d310612fde711ba83",
+    "url": "/img/planets/14.png"
+  },
+  {
+    "revision": "234ef8063ceacb43f9d3040801867c80",
+    "url": "/img/planets/15.png"
+  },
+  {
+    "revision": "f1f227e876e436e3c77c3ca93d16cd71",
+    "url": "/img/planets/16.png"
+  },
+  {
+    "revision": "a898c2ebb42b9fa323017901b6220c27",
+    "url": "/img/planets/2.png"
+  },
+  {
+    "revision": "0dccae492c6ab9c5b9e5e636e7a1acc3",
+    "url": "/img/planets/3.png"
+  },
+  {
+    "revision": "daf9243575e863b792e653d318d7e86d",
+    "url": "/img/planets/4.png"
+  },
+  {
+    "revision": "a644ceef12dc7f86d88bf829e6440045",
+    "url": "/img/planets/5.png"
+  },
+  {
+    "revision": "27c12ad3bbbb35cd09302e1083690ed8",
+    "url": "/img/planets/6.png"
+  },
+  {
+    "revision": "9c528927ff617e6cbcc4e53f9fb5ead6",
+    "url": "/img/planets/7.png"
+  },
+  {
+    "revision": "a759f7680eee60694887fc5bc7a90c57",
+    "url": "/img/planets/8.png"
+  },
+  {
+    "revision": "0432bd2ba4e6a26d26348f6a8b5c6caa",
+    "url": "/img/planets/9.png"
+  },
+  {
+    "revision": "38f01f64099d0ef32f9aa18f820122ec",
+    "url": "/img/screens/1.png"
+  },
+  {
+    "revision": "93c34ab8722953d0097acaef6ae21d52",
+    "url": "/img/screens/2.png"
+  },
+  {
+    "revision": "7c0e796bdf06a60987fe02d6b2919157",
+    "url": "/img/screens/3.png"
+  },
+  {
+    "revision": "160e1658c69b26d4f13e01b474b6fc88",
+    "url": "/img/screens/4.png"
+  },
+  {
+    "revision": "ad55fae8d3e3802cb60a6737a1b8f543",
+    "url": "/img/screens/5.png"
+  },
+  {
+    "revision": "160e1658c69b26d4f13e01b474b6fc88",
+    "url": "/img/screens/6.png"
+  },
+  {
+    "revision": "882026febd3cb81f88ef665858a9a801",
+    "url": "/img/screens/7.png"
+  },
+  {
+    "revision": "93c34ab8722953d0097acaef6ae21d52",
+    "url": "/img/screens/8.png"
+  },
+  {
+    "revision": "0ad38d4301bbb3de8d1cbfd7c463bf69",
+    "url": "/img/screens/9.png"
+  },
+  {
+    "revision": "4e8aa74176922629b263966580a2d4ee",
+    "url": "/img/screens/planet.png"
+  },
+  {
+    "revision": "45ef3ed715df3c2b3a74b7fa99a83a2d",
+    "url": "/img/screens/rocket.png"
+  },
+  {
+    "revision": "f8abf54d3b48e5365e2faaa3b0ce16e6",
+    "url": "/img/screens/ufo.png"
+  },
+  {
+    "revision": "7434d67bc5961b02a7efe847604100c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b6972f4b44c830c9ce4",
+    "url": "/js/app.03301292.js"
+  },
+  {
+    "revision": "8044ffeabf001c653f89",
+    "url": "/js/chunk-2d22fd02.dda296b2.js"
+  },
+  {
+    "revision": "94814d1357d10457d286",
+    "url": "/js/chunk-vendors.76adcfb1.js"
+  },
+  {
+    "revision": "84b128cdb563becd13a9e2b520514574",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
